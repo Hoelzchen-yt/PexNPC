d064e9444c67d3283e19f99ec480e458c56a3b33
Hoelzchen-yt <kitest2012@gmail.com>
2025-12-31 15:01:42 +0100

    Alle Commands auf Deutsch übersetzt ( just for fun )

--- src/main/java/de/pascalpex/pexnpc/commands/subcommands/HelpSubcommand.java
+++ src/main/java/de/pascalpex/pexnpc/commands/subcommands/HelpSubcommand.java
@@ -18,20 +18,20 @@ public class HelpSubcommand implements Command<CommandSourceStack> {
 
         sender.sendMessage(MessageHandler.prefixedMini("PexNPC " + PexNPC.getPluginVersion() + " by Pascalpex"));
         sender.sendMessage(MessageHandler.prefixedMini("Available commands:"));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc help <dark_gray>| <gold>Shows this page").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc help")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc reload <dark_gray>| <gold>Reloads the files and NPCs").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc reload")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc create [NAME] <dark_gray>| <gold>Creates a new NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc create")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc list <dark_gray>| <gold>Shows all NPCs and their IDs").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc list")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc delete [ID] <dark_gray>| <gold>Deletes a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc delete")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc name [ID] [NAME] <dark_gray>| <gold>Changes a name").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc name")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc movehere [ID] <dark_gray>| <gold>Moves a NPC to you").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc movehere")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc skin [ID] [NAME] <dark_gray>| <gold>Changes a skin").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc skin")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc cmd [ID] [CMD] <dark_gray>| <gold>Gives a command to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc cmd")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc msg [ID] [MSG] <dark_gray>| <gold>Gives a message to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc msg")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc item [ID] [SLOT] <dark_gray>| <gold>Gives an item to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc item")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc clear [ID] <dark_gray>| <gold>Clears the command, message and items of a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc clear")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc tp [ID] <dark_gray>| <gold>Teleports to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc tp")));
-        sender.sendMessage(MessageHandler.prefixedMini("/pexnpc inspect <dark_gray>| <gold>Toggles the inspection mode").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/pexnpc inspect")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter hilfe <dark_gray>| <gold>Shows this page").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter hilfe")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter neuladen <dark_gray>| <gold>Reloads the files and NPCs").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter neuladen")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter erstellen [NAME] <dark_gray>| <gold>Creates a new NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter erstellen")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter liste <dark_gray>| <gold>Shows all NPCs and their IDs").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter liste")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter löschen [ID] <dark_gray>| <gold>Deletes a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter löschen")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter name [ID] [NAME] <dark_gray>| <gold>Changes a name").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter name")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter hierhin [ID] <dark_gray>| <gold>Moves a NPC to you").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter hierhin")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter skin [ID] [NAME] <dark_gray>| <gold>Changes a skin").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter skin")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter befehl [ID] [CMD] <dark_gray>| <gold>Gives a command to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter befehl")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter msg [ID] [MSG] <dark_gray>| <gold>Gives a message to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter msg")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter gegenstand [ID] [SLOT] <dark_gray>| <gold>Gives an item to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter gegenstand")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter leeren [ID] <dark_gray>| <gold>Clears the command, message and items of a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter leeren")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter tp [ID] <dark_gray>| <gold>Teleports to a NPC").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter tp")));
+        sender.sendMessage(MessageHandler.prefixedMini("/charakter inspektieren <dark_gray>| <gold>Toggles the inspection mode").hoverEvent(hoverEvent).clickEvent(ClickEvent.suggestCommand("/charakter inspektieren")));
         return SINGLE_SUCCESS;
     }
 }
